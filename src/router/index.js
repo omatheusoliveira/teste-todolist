97e14d71a5a3f1c85df59c0fa391bbce0849625e
Matheus Araujo <matheus-araujo2001@hotmail.com>
2023-03-21 00:29:23 -0300

    hotfix router

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,7 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/PrincipalView.vue'),
+    redirect: "/list-task",
     children: [
       {
         path: '/list-task',
